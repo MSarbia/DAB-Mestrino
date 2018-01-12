d065a96a8c81e17d711f054e84ac8d659d09a5c7
MSarbia <34656486+MSarbia@users.noreply.github.com>
2018-01-12 09:04:19 +0100

    UADMSoapWrap2

---
+++ UADMSoapWrapp/UADMSoapWrapp/DataTypes/OPEXT/MaterialDefinitionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace UADMSoapWrapp.DataTypes.OPEXT
+{
+    [XmlRoot("MaterialDefinitionResponse", Namespace = Constants.Namespace, IsNullable = false)]
+    [DataContract]
+    public class MaterialDefinitionResponse
+    {
+        [DataMember(IsRequired = true)]
+        public string MaterialCode { get; set; }
+        [DataMember(IsRequired = true)]
+        public string MaterialRevision { get; set; }
+    }
+}
