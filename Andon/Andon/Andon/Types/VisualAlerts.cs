83dba1f249437a7142df085220fdf5ccee6c01e3
MSarbia <34656486+MSarbia@users.noreply.github.com>
2018-01-15 15:02:48 +0100

    AndonData types

---
+++ Andon/Andon/Andon/Types/VisualAlerts.cs
@@ -0,0 +1,9 @@
+﻿namespace Engineering.DAB.Andon.Types
+{
+    public class VisualAlerts
+    {
+        public int MaterialCalls { get; set; }
+        public int TeamLeaderCalls { get; set; }
+        public int LineStop { get; set; }
+    }
+}
