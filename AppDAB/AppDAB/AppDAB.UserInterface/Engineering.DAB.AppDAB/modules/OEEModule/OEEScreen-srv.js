1c429da31f74d31db7a300024ca3139894ba682a
Satori1971 <34678710+Satori1971@users.noreply.github.com>
2018-03-05 11:50:04 +0100

    Nuove pagine OEE e Sinottico

--- AppDAB/AppDAB/AppDAB.UserInterface/Engineering.DAB.AppDAB/modules/OEEModule/OEEScreen-srv.js
+++ AppDAB/AppDAB/AppDAB.UserInterface/Engineering.DAB.AppDAB/modules/OEEModule/OEEScreen-srv.js
@@ -101,3 +101,248 @@
         };
     }
 }());
+
+
+//*********************************************************************
+// From DataManager.js
+//
+function drawLineChart(URL_ANDON_GET_DATA, onComplete) {
+
+    if (!URL_ANDON_GET_DATA)
+        URL_ANDON_GET_DATA = "http://localhost/WebserviceAndon/Andon/Andon/GetOEEvalues";
+
+    // Add a helper to format timestamp data
+    Date.prototype.formatMMDDYYYY = function () {
+        return (this.getMonth() + 1) +
+            "/" + this.getDate() +
+            "/" + this.getFullYear();
+    }
+
+    //$.ajax({
+    //    //url: "http://localhost:25412/Andon/Andon/GetOEEvalues",
+    //    url: URL_ANDON_GET_DATA,
+    //    //url: "WebserviceAndon/GetOEEvalues",
+    //    type: 'POST',
+    //    dataType: 'json',
+    //    timeout: 30000,
+    //    cache: false
+    //}).done(function (results) {
+
+    //    // Split timestamp and data into separate arrays
+    //    /*
+    //            var labels = [], data = [];
+    //            results["packets"].forEach(function (packet) {
+    //                labels.push(new Date(packet.timestamp).formatMMDDYYYY());
+    //                data.push(parseFloat(packet.payloadString));
+    //            });
+
+    //            loadProd(labelProd, dataProd);
+    //            loadOEE(labelOEE, dataOEE);
+    //    */
+    loadProd("", "");
+    loadOEE("", "");
+
+    //    $("#gigietto").html(results.ProductionLineId);
+    //    ANDON.SetData()
+    //    onComplete();
+
+    //})
+    //.error(function (err) {
+
+    //    MyTrace(1, "ERR:" + JSON.stringify(err))
+
+    //    try {
+    //        xhr.abort();
+    //    }
+    //    catch (e) { }
+    //    ANDON.timestampAjax = ANDON.getTime();
+    //    ANDON.webServiceStatus = 0;
+    //    $("#ANDON_WS_STATUS").css("background-color", "red");
+    //});
+}
+
+function refreshSynoptic(URL_SYNOPTIC_GET_DATA, onComplete) {
+    if (!URL_SYNOPTIC_GET_DATA)
+        URL_SYNOPTIC_GET_DATA = "http://localhost/WebserviceAndon/Andon/Andon/GetSynopticvalues";
+
+    $.ajax({
+        url: URL_SYNOPTIC_GET_DATA,
+        //url: "WebserviceAndon/GetOEEvalues",
+        type: 'POST',
+        dataType: 'json',
+        timeout: 30000,
+        cache: false
+    }).done(function (results) {
+
+        $("#btnJET").html(results.LineName);
+
+        onComplete();
+
+    })
+    //.error(function (err) {
+
+    //    MyTrace(1, "ERR:" + JSON.stringify(err))
+
+    //    try {
+    //        xhr.abort();
+    //    }
+    //    catch (e) { }
+    //    ANDON.timestampAjax = ANDON.getTime();
+    //    ANDON.webServiceStatus = 0;
+    //    $("#ANDON_WS_STATUS").css("background-color", "red");
+    //});
+}
+
+
+//*********************************************************************
+// From util.js
+//
+'use strict';
+
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+};
+
+(function (global) {
+    var Months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+    ];
+
+    var COLORS = [
+		'#4dc9f6',
+		'#f67019',
+		'#f53794',
+		'#537bc4',
+		'#acc236',
+		'#166a8f',
+		'#00a950',
+		'#58595b',
+		'#8549ba'
+    ];
+
+    var Samples = global.Samples || (global.Samples = {});
+    var Color = global.Color;
+
+    Samples.utils = {
+        // Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+        srand: function (seed) {
+            this._seed = seed;
+        },
+
+        rand: function (min, max) {
+            var seed = this._seed;
+            min = min === undefined ? 0 : min;
+            max = max === undefined ? 1 : max;
+            this._seed = (seed * 9301 + 49297) % 233280;
+            return min + (this._seed / 233280) * (max - min);
+        },
+
+        numbers: function (config) {
+            var cfg = config || {};
+            var min = cfg.min || 0;
+            var max = cfg.max || 1;
+            var from = cfg.from || [];
+            var count = cfg.count || 8;
+            var decimals = cfg.decimals || 8;
+            var continuity = cfg.continuity || 1;
+            var dfactor = Math.pow(10, decimals) || 0;
+            var data = [];
+            var i, value;
+
+            for (i = 0; i < count; ++i) {
+                value = (from[i] || 0) + this.rand(min, max);
+                if (this.rand() <= continuity) {
+                    data.push(Math.round(dfactor * value) / dfactor);
+                } else {
+                    data.push(null);
+                }
+            }
+
+            return data;
+        },
+
+        labels: function (config) {
+            var cfg = config || {};
+            var min = cfg.min || 0;
+            var max = cfg.max || 100;
+            var count = cfg.count || 8;
+            var step = (max - min) / count;
+            var decimals = cfg.decimals || 8;
+            var dfactor = Math.pow(10, decimals) || 0;
+            var prefix = cfg.prefix || '';
+            var values = [];
+            var i;
+
+            for (i = min; i < max; i += step) {
+                values.push(prefix + Math.round(dfactor * i) / dfactor);
+            }
+
+            return values;
+        },
+
+        months: function (config) {
+            var cfg = config || {};
+            var count = cfg.count || 12;
+            var section = cfg.section;
+            var values = [];
+            var i, value;
+
+            for (i = 0; i < count; ++i) {
+                value = Months[Math.ceil(i) % 12];
+                values.push(value.substring(0, section));
+            }
+
+            return values;
+        },
+
+        color: function (index) {
+            return COLORS[index % COLORS.length];
+        },
+
+        transparentize: function (color, opacity) {
+            var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+            return Color(color).alpha(alpha).rgbString();
+        }
+    };
+
+    // DEPRECATED
+    window.randomScalingFactor = function () {
+        return Math.round(Samples.utils.rand(0, 100));
+    };
+
+    // INITIALIZATION
+
+    Samples.utils.srand(Date.now());
+
+    // Google Analytics
+    /* eslint-disable */
+    if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-28909194-3', 'auto');
+        ga('send', 'pageview');
+    }
+    /* eslint-enable */
+
+}(this));
--- AppDAB/AppDAB/AppDAB.UserInterface/Engineering.DAB.AppDAB/modules/SynopticModule/SynopticScreen-list-ctrl.js
+++ AppDAB/AppDAB/AppDAB.UserInterface/Engineering.DAB.AppDAB/modules/SynopticModule/SynopticScreen-list-ctrl.js
@@ -14,8 +14,8 @@
             logger = loggerService.getModuleLogger('Engineering.DAB.AppDAB.SynopticModule.SynopticScreen');
 
             init();
-            initGridOptions();
-            initGridData();
+//            initGridOptions();
+//            initGridData();
         }
 
         function init() {
@@ -36,6 +36,8 @@
             self.editButtonHandler = editButtonHandler;
             self.selectButtonHandler = selectButtonHandler;
             self.deleteButtonHandler = deleteButtonHandler;
+
+            myRefresh();
         }
 
         function initGridOptions() {
@@ -139,3 +141,174 @@
         $stateProvider.state(state);
     }
 }());
+
+
+//*********************************************************************
+// From DataManager.js
+//
+function drawLineChart(URL_ANDON_GET_DATA, onComplete) {
+
+    if (!URL_ANDON_GET_DATA)
+        URL_ANDON_GET_DATA = "http://localhost/WebserviceAndon/Andon/Andon/GetOEEvalues";
+
+    // Add a helper to format timestamp data
+    Date.prototype.formatMMDDYYYY = function () {
+        return (this.getMonth() + 1) +
+            "/" + this.getDate() +
+            "/" + this.getFullYear();
+    }
+
+    //$.ajax({
+    //    //url: "http://localhost:25412/Andon/Andon/GetOEEvalues",
+    //    url: URL_ANDON_GET_DATA,
+    //    //url: "WebserviceAndon/GetOEEvalues",
+    //    type: 'POST',
+    //    dataType: 'json',
+    //    timeout: 30000,
+    //    cache: false
+    //}).done(function (results) {
+
+    //    // Split timestamp and data into separate arrays
+    //    /*
+    //            var labels = [], data = [];
+    //            results["packets"].forEach(function (packet) {
+    //                labels.push(new Date(packet.timestamp).formatMMDDYYYY());
+    //                data.push(parseFloat(packet.payloadString));
+    //            });
+
+    //            loadProd(labelProd, dataProd);
+    //            loadOEE(labelOEE, dataOEE);
+    //    */
+    loadProd("", "");
+    loadOEE("", "");
+
+    //    $("#gigietto").html(results.ProductionLineId);
+    //    ANDON.SetData()
+    //    onComplete();
+
+    //})
+    //.error(function (err) {
+
+    //    MyTrace(1, "ERR:" + JSON.stringify(err))
+
+    //    try {
+    //        xhr.abort();
+    //    }
+    //    catch (e) { }
+    //    ANDON.timestampAjax = ANDON.getTime();
+    //    ANDON.webServiceStatus = 0;
+    //    $("#ANDON_WS_STATUS").css("background-color", "red");
+    //});
+}
+
+function refreshSynoptic(URL_SYNOPTIC_GET_DATA, onComplete) {
+    if (!URL_SYNOPTIC_GET_DATA)
+        URL_SYNOPTIC_GET_DATA = "http://localhost/WebserviceAndon/Andon/Andon/GetSynopticvalues";
+
+    $.ajax({
+        url: URL_SYNOPTIC_GET_DATA,
+        //url: "WebserviceAndon/GetOEEvalues",
+        type: 'POST',
+        dataType: 'json',
+        timeout: 30000,
+        cache: false
+    }).done(function (results) {
+
+        $("#btnJET").html(results.LineName);
+
+        onComplete();
+
+    })
+    //.error(function (err) {
+
+    //    MyTrace(1, "ERR:" + JSON.stringify(err))
+
+    //    try {
+    //        xhr.abort();
+    //    }
+    //    catch (e) { }
+    //    ANDON.timestampAjax = ANDON.getTime();
+    //    ANDON.webServiceStatus = 0;
+    //    $("#ANDON_WS_STATUS").css("background-color", "red");
+    //});
+}
+
+//*********************************************************************
+// From Synoptic.js
+//
+jQuery(window).ready(function () {
+    var modalTriggerBts = $('a[data-type="cd-modal-trigger"]'),
+        coverLayer = $('#mainContent');
+
+    modalTriggerBts.each(function () {
+        initModal($(this));
+    });
+
+    function initModal(modalTrigger) {
+        var modalTriggerId = modalTrigger.attr('id'),
+            modal = $('.cd-modal[data-modal="' + modalTriggerId + '"]');
+        svgCoverLayer = modal.children('.cd-svg-bg'),
+        //paths = svgCoverLayer.find('path'),
+        //pathsArray = [];
+        //store Snap objects
+        //pathsArray[0] = Snap('#' + paths.eq(0).attr('id')),
+        //    pathsArray[1] = Snap('#' + paths.eq(1).attr('id')),
+        //    pathsArray[2] = Snap('#' + paths.eq(2).attr('id'));
+
+        //store path 'd' attribute values	
+        //var pathSteps = [];
+        //pathSteps[0] = svgCoverLayer.data('step1');
+        //pathSteps[1] = svgCoverLayer.data('step2');
+        //pathSteps[2] = svgCoverLayer.data('step3');
+        //pathSteps[3] = svgCoverLayer.data('step4');
+        //pathSteps[4] = svgCoverLayer.data('step5');
+        //pathSteps[5] = svgCoverLayer.data('step6');
+
+        //open modal window
+    modalTrigger.on('click', function (event) {
+        event.preventDefault();
+        modal.addClass('modal-is-visible');
+        coverLayer.addClass('modal-is-visible');
+        //animateModal(pathsArray, pathSteps, duration, 'open');
+    });
+
+        //close modal window
+        modal.on('click', '.modal-close', function (event) {
+            event.preventDefault();
+            modal.removeClass('modal-is-visible');
+            coverLayer.removeClass('modal-is-visible');
+            //animateModal(pathsArray, pathSteps, duration, 'close');
+        });
+    }
+
+    myScale();
+    //    myRefresh();      //moved in function init()
+
+});
+
+function myRefresh() {
+    //refreshSynoptic(function () { setTimeout("myRefresh()", 1000) });
+
+    jQuery("#btnJET").html("<div><b>Linea JET</b><br />OPR: 10<br />LE: 100<br /></div>");
+    jQuery("#btnM80").html("<div><b>Linea M80</b><br />OPR: 10<br />LE: 100<br /></div>");
+    jQuery("#btnM100").html("<div><b>Linea M100</b><br />OPR: 10<br />LE: 100<br /></div>");
+
+    $("#btnM100").remove("buttonStyle");
+    $("#btnM100").addClass("buttonAlarmsStyle");
+
+
+    $("#qtaImpostata").innerHTML = "<text>cambio testo</text>";
+}
+
+function myScale() {
+    if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
+    {
+        var wWin = window.innerWidth;
+        var hWin = window.innerHeight;
+
+        $("#svgSynoptic").width(wWin - 32);
+        $("#svgSynoptic").height(hWin - 32);
+        $("#bgSynoptic").width(wWin - 32);
+        $("#bgSynoptic").height(hWin - 32);
+    }
+}
