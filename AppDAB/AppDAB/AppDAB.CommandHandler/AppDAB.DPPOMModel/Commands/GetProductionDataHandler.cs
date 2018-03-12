73d3878c5b395b6cc15865630fcc46fe0d19a469
diej999 <dg.sivori@gmail.com>
2018-03-12 16:23:51 +0100

    Fixed filter toDate to 23:59:59

--- AppDAB/AppDAB/AppDAB.CommandHandler/AppDAB.DPPOMModel/Commands/GetProductionDataHandler.cs
+++ AppDAB/AppDAB/AppDAB.CommandHandler/AppDAB.DPPOMModel/Commands/GetProductionDataHandler.cs
@@ -27,10 +27,11 @@ namespace Engineering.DAB.AppDAB.AppDAB.DPPOMModel.Commands
         private GetProductionData.Response GetProductionDataHandler(GetProductionData command)
         {
             DateTime today = DateTime.UtcNow;
+            var todayFilter = new DateTime(today.Year, today.Month, today.Day, 23, 59, 59);
             var response = new GetProductionData.Response(new List<Types.GraphPoint>(), new List<Types.GraphPoint>());
             for (int d = 0; d < 15; d++)
             {
-                var toDate = today.AddDays(-d);
+                var toDate = todayFilter.AddDays(-d);
                 var fromDate = toDate.AddDays(-1);
                 var pis = Platform.CallCommand<GetProductionInfo, GetProductionInfo.Response>(new GetProductionInfo { WorkArea = command.WorkArea, FromDate = fromDate, ToDate = toDate });
                 var producedPieces = Platform.ProjectionQuery<DailyProduction>().Where(p => p.WorkArea == command.WorkArea && p.Year == toDate.Year && p.Month == toDate.Month && p.Day == toDate.Day).Select(p => p.Pieces).DefaultIfEmpty(0).Sum();
